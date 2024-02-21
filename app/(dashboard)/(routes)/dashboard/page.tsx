f06c83ff4dbe62c5917b3674423da03409523581
jdu211171 <211171@jdu.uz>
2024-02-21 16:55:45 +0500

    Sidebar finished, Redux started

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Sidebar} from "@/components/sidebar";
+
+const DashboardPage = () => {
+    return (
+            // <Sidebar />
+        <div>
+            <p>All Posts Here</p>
+        </div>
+    );
+}
+
+export default DashboardPage;
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,16 @@
+// @flow
+import * as React from 'react';
+
+type Props = {
+
+};
+
+const LandingPage = (props: Props) => {
+    return (
+        <div>
+            Landing Page (Unprotected)
+        </div>
+    );
+};
+
+export default LandingPage;
